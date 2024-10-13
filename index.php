a1e5f1b88cc2087ff17f6e198f787ea0b5654c4c
themakerbb <borja.mz8@gmail.com>
2024-10-13 14:39:30 +0200

    daw

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba</title>
+</head>
+<body>
+    <h1>Borja Molina Zea</h1>
+    <?php
+        date_default_timezone_set('Europe/Madrid');
+        echo date('H:i:s');
+    ?>
+    <div id="relojCliente"></div>
+    <form action="insertar.php" method="POST">
+        <label for="nombre">Nombre:</label>
+        <input type="text" id="nombre" name="nombre" required>
+        <br>
+        <label for="edad">Edad:</label>
+        <input type="number" name="edad" id="edad" required>
+        <br>
+        <input type="sumbit" value="Enviar">
+    </form>
+    <script src="js/relojs.js"></script>
+</body>
+</html>
---
+++ insertar.php
@@ -0,0 +1,29 @@
+<?php
+    $servername = "localhost:8888"; 
+    $username = "bor";
+    $password = "bor"; //Soy consciente de que estoy poniendo una pass, es para hacer pruebas de despliegue en local para fines didácticos       
+    $dbname = "prueba"; 
+
+    // Crear conexión con la base de datos
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Verificar la conexión
+    if ($conn->connect_error) {
+        die("Error en la conexión: " . $conn->connect_error);
+    }
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $nombre = $_POST['nombre'];
+        $edad = $_POST['edad'];
+        $sql = "INSERT INTO persona (nombre, edad) VALUES ('$nombre', '$edad')";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "Datos insertados correctamente";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+    }
+
+    // Cerrar la conexión a la base de datos
+    $conn->close();
+?>
